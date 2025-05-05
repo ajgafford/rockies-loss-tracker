7ac9eaac0dc36fcc0c327b080e2054ab8c28bad1
Andrew Joseph Gafford <andrew.gafford@gmail.com>
2025-05-05 13:30:03 -0500

    Update run_scripts.py

--- Scripts/run_scripts.py
+++ Scripts/run_scripts.py
@@ -1,5 +1,12 @@
 import subprocess
+import os
 
-subprocess.run(["/usr/bin/python3", "/Users/ajgafford/Documents/Projects/Reached_on_Error/Rockies_Loss_Tracker/Scripts/scrape_games.py"])
-subprocess.run(["/usr/bin/python3", "/Users/ajgafford/Documents/Projects/Reached_on_Error/Rockies_Loss_Tracker/Scripts/merge_schedules.py"])
-subprocess.run(["streamlit", "run", "/Users/ajgafford/Documents/Projects/Reached_on_Error/Rockies_Loss_Tracker/Scripts/rockies_tracker.py"])
+base_dir = os.path.dirname(os.path.abspath(__file__))
+
+scrape_games_path = os.path.join(base_dir, 'Scripts', 'scrape_games.py')
+merge_schedules_path = os.path.join(base_dir, 'Scripts', 'merge_schedules.py')
+app_path = os.path.join(base_dir, 'app.py')
+
+subprocess.run(['/usr/bin/python3', scrape_games_path])
+subprocess.run(['/usr/bin/python3', merge_schedules_path])
+subprocess.run(['/usr/bin/python3', app_path])
