ee9d83e223e535bcaa81e77dceb9a7fc151d1871
Andrew Joseph Gafford <andrew.gafford@gmail.com>
2025-05-05 13:47:30 -0500

    Update run_scripts.py and create cron.log

--- Scripts/run_scripts.py
+++ Scripts/run_scripts.py
@@ -3,9 +3,9 @@ import os
 
 base_dir = os.path.dirname(os.path.abspath(__file__))
 
-scrape_games_path = os.path.join(base_dir, 'Scripts', 'scrape_games.py')
-merge_schedules_path = os.path.join(base_dir, 'Scripts', 'merge_schedules.py')
-app_path = os.path.join(base_dir, 'app.py')
+scrape_games_path = os.path.join(base_dir, 'scrape_games.py')
+merge_schedules_path = os.path.join(base_dir, 'merge_schedules.py')
+app_path = os.path.join(base_dir, '../app.py')
 
 subprocess.run(['/usr/bin/python3', scrape_games_path])
 subprocess.run(['/usr/bin/python3', merge_schedules_path])
