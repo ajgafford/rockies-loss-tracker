2e7833f881584ddc028546ae5658fcaf1c3634b6
Andrew Joseph Gafford <andrew.gafford@gmail.com>
2025-05-08 09:53:00 -0500

    Create run_tracker.sh
    shell command to run in the terminal

---
+++ run_tracker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Activate the virtual environment
+source /Users/ajgafford/Documents/Projects/Reached_on_Error/Rockies_Loss_Tracker/venv/bin/activate
+
+# Run the update script
+python /Users/ajgafford/Documents/Projects/Reached_on_Error/Rockies_Loss_Tracker/Scripts/run_scripts.py
