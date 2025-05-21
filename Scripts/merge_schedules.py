ec4b67f1cc0d2a68bb049b4a9f5bff8acd5da78e
Andrew Joseph Gafford <andrew.gafford@gmail.com>
2025-05-21 04:16:26 -0500

    Update merge_schedules.py
    Include the 1932 Boston Red Sox

--- Scripts/merge_schedules.py
+++ Scripts/merge_schedules.py
@@ -10,24 +10,27 @@ processed_data_dir = os.path.join(project_root, 'Data', 'Processed')
 rockies = pd.read_csv(os.path.join(raw_data_dir, 'COL_2025_schedule.csv'))
 sox = pd.read_csv(os.path.join(raw_data_dir, 'CHW_2024_schedule.csv'), usecols=['Gm#', 'W/L', 'R', 'RA'])
 mets = pd.read_csv(os.path.join(raw_data_dir, 'NYM_1962_schedule.csv'), usecols=['Gm#', 'W/L', 'R', 'RA'])
+bosox = pd.read_csv(os.path.join(raw_data_dir, 'BOS_1932_schedule.csv'), usecols=['Gm#', 'W/L', 'R', 'RA'])
 
 # rename columns in sox and mets
-for team in [sox, mets]:
+for team in [sox, mets, bosox]:
     team.rename(columns={'Gm#': 'Games Played', 'W/L': 'Result'}, inplace=True)
 
 # add in the team column
 rockies['Team'] = '2025 COL'
 sox['Team'] = '2024 CHW'
 mets['Team'] = '1962 NYM'
+bosox['Team'] = '1932 BOS'
 
 # add in columns for additional features
-for team in [rockies, sox, mets]:
+for team in [rockies, sox, mets, bosox]:
     team['Loss?'] = team['Result'].str.startswith('L').astype('int')  # will assign 1 if started with an 'L'
     team['Losses'] = team['Loss?'].cumsum()  # add all previous values
     team['Expected Losses'] = round(
         team['Games Played'] * (1 - team['R'].cumsum() ** 2 / (team['R'].cumsum() ** 2 + team['RA'].cumsum() ** 2))
     ).astype(int)  # using the Pythag W/L Record
+    team['RD'] = team['R'].cumsum() - team['RA'].cumsum() # track the cumulative Run Differential
 
 # join everyone together
-losers = pd.concat([rockies, sox, mets], ignore_index=True)
+losers = pd.concat([rockies, sox, mets, bosox], ignore_index=True)
 losers.to_csv(os.path.join(processed_data_dir, 'losers.csv'), index=False)
