0aa8b5a009c143982403f2311a3939bcf784fd06
Andrew Joseph Gafford <andrew.gafford@gmail.com>
2025-05-21 04:57:56 -0500

    Update app.py

--- app.py
+++ app.py
@@ -88,6 +88,7 @@ st.markdown("""
 st.subheader("Losses by Games Played")
 game_limit = st.slider("Show up to game #:", 1, max_games, max_games)
 
+filtered_df = df[df['Team'] != '1932 BOS']
 filtered_df = df[df['Games Played'] <= game_limit]
 
 # LINE CHART
